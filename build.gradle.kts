c29bb3dca43bd60e42f24940c6a78ba59deefb43
Pedro Luis <pedroluisnr22@gmail.com>
2024-11-09 19:31:18 -0500

    build: :package: add h2 db dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
@@ -25,6 +24,7 @@ dependencies {
     implementation(libs.ktor.server.jetty.jakarta)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+    implementation(libs.h2.database.engine)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
