00703aa3afb1b33a24f0bfae606ba93ab1b19b72
Pedro Luis <pedroluisnr22@gmail.com>
2024-11-09 23:09:31 -0500

    feat: :sparkles: upload and save customer data file

--- src/main/kotlin/com/github/pedroluis02/dbdataloading/plugins/Routing.kt
+++ src/main/kotlin/com/github/pedroluis02/dbdataloading/plugins/Routing.kt
@@ -1,8 +1,14 @@
 package com.github.pedroluis02.dbdataloading.plugins
 
+import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.utils.io.*
+import kotlinx.io.readByteArray
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
@@ -12,6 +18,19 @@ fun Application.configureRouting() {
 
         get("/json/jackson") {
             call.respond(mapOf("hello" to "world"))
+        }
+
+        post("/api/v1/customer") {
+            val multiPart = call.receiveMultipart()
+            val part = multiPart.readPart()
+            if (part is PartData.FileItem) {
+                val name = part.originalFileName as String
+                File("${System.currentTimeMillis()}-$name")
+                    .writeBytes(part.provider().readRemaining().readByteArray())
+                part.dispose()
+            }
+
+            call.respond(HttpStatusCode.OK)
         }
     }
 }
